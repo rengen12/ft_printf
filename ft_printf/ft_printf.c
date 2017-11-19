534dad7ac901d067bed9f44e8276a6210cb21665
rengen12 <rengen12@gmail.com>
2017-11-19 23:22:14 +0200

    notebook

---
+++ ft_printf/ft_printf.c
@@ -0,0 +1,7 @@
+#include <stdarg.h>
+#include "libft/libft.h"
+
+int ft_printf (const char *format, ...)
+{
+    
+}
--- ft_printf/main.c
+++ ft_printf/main.c
@@ -1,11 +1,6 @@
 #include <stdio.h>
 
 
-int ft_printf (const char *format, ...)
-{
-
-}
-
 int main() {
     printf("Hello, World!\n");
     return 0;

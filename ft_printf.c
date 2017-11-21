40616673cead54300d858172127ef9be9a695fac
Andrii MICHAK <amichak@e2r9p1.unit.ua>
2017-11-21 18:32:46 +0200

    start

--- ft_printf.c
+++ ft_printf.c
@@ -1,34 +1,20 @@
 #include <stdarg.h>
 #include "libft/libft.h"
 
+size_t handstr(va_listap, size_t i){
+
+    return ();
+}
+
 int ft_printf (const char *format, ...)
 {
-    int     i;
+    size_t  i;
     va_list ap;
-    char *n;
-    int n2;
 
-    va_start(ap, i);
     if (!format)
         return (0);
-    ft_putendl("i");
-    ft_putnbr(i);
-    ft_putendl("");
-    ft_putendl("");
-    n = va_arg(ap, char *);
-    ft_putendl("");
-    ft_putchar(n);
-    n = va_arg(ap, int);
-    ft_putnbr(n);
-    /*while(format[i])
-    {
-        if (format[i] == '%')
-        {
-            if (format[i + 1] == '%')
-                ft_putchar('%');
-            ft_putchar(va_arg());
-        }
-        ft_putchar(format[i++]);
-    }*/
+    va_start(ap, i);
+    i = handstr(ap, i);
+    va_end(ap);
     return (i);
 }
